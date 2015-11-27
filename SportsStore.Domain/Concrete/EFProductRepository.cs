825d2629bb0c00b1ba25c66e1fb429fea8fc91a0
Taras <taras.pyvovarov@globallogic.com>
2015-11-27 14:11:37 +0200

    Added initial db content creation

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 
@@ -14,6 +15,34 @@ namespace SportsStore.Domain.Concrete
             {
                 return _context.Products;
             }
+        }
+
+        public EFProductRepository()
+        {
+            var productsCount = Products.Count();
+            if (productsCount == 0)
+            {
+                AddNewProduct("Kayak", "A boat for one person", "Watersports", 275);
+                AddNewProduct("Lifejacket", "Protective and fashionable", "Watersports", 48.95M);
+                AddNewProduct("Soccer Ball", "FIFA-approved size and weight", "Soccer", 19.50M);
+                AddNewProduct("Corner Flags", "Give your playing field a professional touch", "Soccer", 34.95M);
+                AddNewProduct("Stadium", "Flat-packed 35,000-seat stadium", "Soccer", 79500);
+                AddNewProduct("Thinking Cap", "Improve your brain efficiency by 75%", "Chess", 16);
+                AddNewProduct("Unsteady Chair", "Secretly give your opponent a disadvantage", "Chess", 29.95M);
+                AddNewProduct("Human Chess Board", "A fun game for the family", "Chess", 75);
+                AddNewProduct("Bling-Bling-Bling", "Gold-plated, diamond-studded King", "Chess", 1200);
+            }
+        }
+
+        private void AddNewProduct(string name, string description, string category, decimal price)
+        {
+            Product p = new Product();
+            p.Name = name;
+            p.Description = description;
+            p.Category = category;
+            p.Price = price;
+            _context.Products.Add(p);
+            _context.SaveChanges();
         }
     }
 }
