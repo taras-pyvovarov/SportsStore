ab8ec4f50f3529c487952dbef3c32919a6100b31
Taras <taras.pyvovarov@globallogic.com>
2015-11-27 17:57:11 +0200

    Added initial admin views

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class AdminController : Controller
     {
-        //
-        // GET: /Admin/
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
 
         public ActionResult Index()
         {
-            return View();
+            return View(_repository.Products);
         }
 
+        public ViewResult Edit(int? productId)
+        {
+            Product product = _repository.Products.Single(x => x.ProductID == productId);
+            return View(product);
+        }
     }
 }
